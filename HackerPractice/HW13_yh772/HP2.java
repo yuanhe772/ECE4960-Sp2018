4e9cdd3d3c5cae97186549db75f0c6a90b602228
yuanhe772 <31265339+yuanhe772@users.noreply.github.com>
2018-05-06 21:18:32 -0400

    Add files via upload

--- HackerPractice/HW13_yh772/HP2.java
+++ HackerPractice/HW13_yh772/HP2.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 /**
  * HP2.java, ECE4960-HW13
@@ -8,15 +9,28 @@
  * Note 7(2), page 25
  */
 public class HP2 {
-	
-	
-
-	
+	/* Class Variants: */
 	// Matrix A:
-	double[] valueA = {-2,1, 1,-2,1, 1,-2,1, 1,-2,1, 1,-2,1, 1,-2,1, 1,-2,1, 1,-2,1, 1,-2};
-	int[] rowPtrA = {0, 2, 5, 8, 11, 14, 17, 20, 23, 25};
-	int[] colIndA = {0, 1, 0, 1, 2, 1, 2, 3, 2,3,4,3,4,5,4,5,6,5,6,7,6,7,8,8,9};
-	SparseMatrix A = new SparseMatrix(valueA, rowPtrA, colIndA);
-	
-	
+	static int[] rowPtrA = { 0, 2, 5, 8, 11, 14, 17, 20, 23, 25 };
+	static int[] colIndA = { 0,1, 0,1,2, 1,2,3, 2,3,4, 3,4,5, 4,5,6, 5,6,7, 6,7,8, 7,8};
+	static double[] valueA = { -2,1, 1,-2,1, 1,-2,1, 1,-2,1, 1,-2,1, 1,-2,1, 1,-2,1, 1,-2,1, 1,-2 };
+	static SparseMatrix A = new SparseMatrix(valueA, rowPtrA, colIndA);
+
+	/* Class Methods: */
+	/** A * PHI = h^2 * f, Want to solve PHI */
+	public static Vector finiteElement(int N, double h) {
+		double h2 = h*h;
+		// Vector f
+		double[] f = new double[N-1];
+		for (int i = 0; i < f.length; i++) {f[i] = -1 * (h2);}			
+		Vector PHI = Jacobi.solver(A,new Vector(f));
+		return PHI;
+	}
+
+	public static void main(String[] args) {
+		int N = 10;
+		double h = (double) 1/N;
+		Vector PHI = finiteElement(N, h);
+		System.out.println("PHI = "+Arrays.toString(PHI.v)+"\n,  which is consistent with previous Hacker Practices.");
+	}
 }
